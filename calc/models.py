e6208c1cddd1fe85ffe4f71aa489d16838a4eea7
yesing <yesing@daum.net>
2022-01-04 03:00:43 +0900

    JSON serializer done

--- calc/models.py
+++ calc/models.py
@@ -5,5 +5,5 @@ from django.conf import settings
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- calc/serializers.py
+++ calc/serializers.py
@@ -1,8 +1,27 @@
-from rest_framework.serializers import ModelSerializer
+#from django.conf.auth import get_user_model
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(ModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='author.username')
+    author_email = serializers.ReadOnlyField(source='author.email')
+    author = AuthorSerializer
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = [
+            'pk',
+            'username',
+            'author',
+            'author_email',
+            'message',
+            'created_at',
+            'updated_at',
+        ]
