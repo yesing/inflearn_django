0d785df83e5fd21cc6b72904a3123cb20bab4c9c
yesing <yesing@daum.net>
2022-01-04 02:50:20 +0900

    add author(working)

--- calc/models.py
+++ calc/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
